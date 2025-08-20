b73b49bd46422ab2744e26ac679ccc6b7b20ca61
Satyanarayan Patra <Satyanarayan@indpro.se>
2025-08-21 02:23:57 +0530

    day-2 done

---
+++ attachments/02-essentials/01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  imgPath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  selectedUser = signal(
+    DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]
+  );
+
+  // get imgPath() {
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
+
+  onSelectUser() {
+    this.selectedUser.set(
+      DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]
+    );
+  }
+}
